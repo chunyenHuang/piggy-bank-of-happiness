191d348955ae578a8c74ba40076b56db31e3209b
John Huang <little78926@gmail.com>
2020-06-24 02:41:48 -0700

    Add user sync

--- data/admin/sync.js
+++ data/admin/sync.js
@@ -10,6 +10,10 @@ AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: AWS_PROFILE
 const cognito = new AWS.CognitoIdentityServiceProvider({ region: AWS_REGION });
 
 // sync CompanyUser with cognito
+
+// add user to the test org automatically
+const DEFAULT_ORG_ID = '58e127ce-b36c-11ea-b3de-0242ac130004';
+
 (async () => {
   try {
     const {
@@ -28,10 +32,6 @@ const cognito = new AWS.CognitoIdentityServiceProvider({ region: AWS_REGION });
         return obj;
       }, {});
 
-      if (!attributes['custom:organizationId']) {
-        return;
-      }
-
       const { Groups: groups } = await cognito.adminListGroupsForUser({
         UserPoolId: COGNITO_POOLS_ID,
         Username: user.Username,
@@ -39,8 +39,36 @@ const cognito = new AWS.CognitoIdentityServiceProvider({ region: AWS_REGION });
 
       const groupName = groups.map(({ GroupName }) => GroupName).filter((x) => !x.includes('_'))[0];
 
+      // User does not have a valid group, add to the OrgAdmins
+      if (!groupName) {
+        const addParams = {
+          GroupName: 'OrgAdmins',
+          UserPoolId: COGNITO_POOLS_ID,
+          Username: user.Username,
+        };
+        await cognito.adminAddUserToGroup(addParams).promise();
+      }
+
+      if (!attributes['custom:organizationId']) {
+        const updateParams = {
+          UserAttributes: [
+            {
+              Name: 'custom:organizationId',
+              Value: DEFAULT_ORG_ID,
+            },
+            {
+              Name: 'custom:organizationName',
+              Value: '小豬銀行',
+            }],
+          UserPoolId: COGNITO_POOLS_ID,
+          Username: user.Username,
+        };
+
+        await cognito.adminUpdateUserAttributes(updateParams).promise();
+      }
+
       toUpdateData.push({
-        organizationId: attributes['custom:organizationId'],
+        organizationId: attributes['custom:organizationId'] || DEFAULT_ORG_ID,
         username: user.Username,
         idNumber: uuidv1(),
         name: attributes['name'],
